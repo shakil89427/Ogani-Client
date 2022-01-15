a0cc50cdbcdc58739b37bd9df4b8d95fcad73d62
shakil89427 <shakilahmed89427@gmail.com>
2022-01-15 14:36:35 +0600

    cart calculation

--- src/Components/AuthProvider/Store.js
+++ src/Components/AuthProvider/Store.js
@@ -13,29 +13,20 @@ const Store = () => {
   const setToLocal = (value) => {
     localStorage.setItem("cart", JSON.stringify(value));
     setCartItems(value);
-    setLoading(false);
   };
 
   /* Add single quantity */
-  const addSingleQuantity = (id, value) => {
+  const addSingleQuantity = (id) => {
     const price = allProducts.find((single) => single._id === id);
     const matched = cartItems.products.find(
       (single) => single.productId === id
     );
     if (!matched) {
       const product = { quantity: 1, productId: id, price: price.price };
-      if (value) {
-        product.quantity = value;
-      }
       cartItems.products.push(product);
       return setToLocal(cartItems);
     }
-    if (value) {
-      matched.quantity = matched.quantity + value;
-    }
-    if (!value) {
-      matched.quantity = matched.quantity + 1;
-    }
+    matched.quantity = matched.quantity + 1;
     const result = cartItems.products.filter(
       (single) => single.productId !== id || matched
     );
@@ -74,6 +65,7 @@ const Store = () => {
     setSearchValue,
     cartItems,
     addSingleQuantity,
+    setToLocal,
   };
 };
 
--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,11 +2,15 @@ import React, { useEffect, useState } from "react";
 import useAuth from "../AuthProvider/useAuth";
 import { Col, Container, Row } from "react-bootstrap";
 import "./Cart.css";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
-  const { loading, allProducts, cartItems } = useAuth();
+  const { allProducts, cartItems, setToLocal } = useAuth();
   const [products, setProducts] = useState([]);
+  const [price, setPrice] = useState(0);
+  const [tax, setTax] = useState(0);
 
+  /* Load Cart items */
   useEffect(() => {
     if (cartItems.user) {
       const temp = [];
@@ -21,6 +25,47 @@ const Cart = () => {
       setProducts(temp);
     }
   }, [cartItems]);
+
+  /* Change product quantity */
+  const changeQuantity = (id, exist) => {
+    let temp = [];
+    for (const product of products) {
+      if (product.productId === id && exist) {
+        product.quantity = product.quantity + 1;
+        temp.push(product);
+      } else if (product.productId === id && !exist) {
+        if (product.quantity === 1) return;
+        product.quantity = product.quantity - 1;
+        temp.push(product);
+      } else {
+        temp.push(product);
+      }
+    }
+    setProducts(temp);
+  };
+
+  /* Remove product from cart */
+  const removefromCart = (id) => {
+    const result = products.filter((single) => single.productId !== id);
+    setProducts(result);
+  };
+
+  /* Finalize the cart */
+  const update = () => {
+    cartItems.products = products;
+    setToLocal(cartItems);
+  };
+
+  /* Calculation */
+  useEffect(() => {
+    let temp = 0;
+    for (const product of products) {
+      temp = temp + product.price * product.quantity;
+    }
+    const tempTax = temp * 0.2;
+    setPrice(parseFloat(temp).toFixed(2));
+    setTax(parseFloat(tempTax).toFixed(2));
+  }, [products]);
   return (
     <div>
       <h1 className="text-center fw-bold cart-h1">Shopping Cart</h1>
@@ -29,12 +74,16 @@ const Cart = () => {
           <Col xs={4} sm={4} md={6} lg={6}>
             <h5 className="fw-bolder">Products</h5>
           </Col>
-          <Col xs={8} sm={8} md={6} lg={6}>
-            <div className="d-flex align-items-center justify-content-between">
-              <h5 className="fw-bolder">Price</h5>
-              <h5 className="fw-bolder">Quantity</h5>
-              <h5 className="fw-bolder">Total</h5>
-            </div>
+          <Col
+            className="d-flex align-items-center justify-content-between"
+            xs={8}
+            sm={8}
+            md={6}
+            lg={6}
+          >
+            <h5 className="fw-bolder">Price</h5>
+            <h5 className="fw-bolder">Quantity</h5>
+            <h5 className="fw-bolder">Total</h5>
           </Col>
         </Row>
 
@@ -42,21 +91,45 @@ const Cart = () => {
           <Row className="border-bottom py-2">
             <Col xs={4} sm={4} md={6} lg={6}>
               <div className="d-flex flex-column flex-md-row align-items-center">
-                <img className="cart-img" src={each.img} alt="" />
-                <h5>{each.name}</h5>
+                <button
+                  onClick={() => removefromCart(each.productId)}
+                  className="border-0 me-2 mb-2"
+                >
+                  X
+                </button>
+                <img className="cart-img me-3 mb-2" src={each.img} alt="" />
+                <h6>{each.name}</h6>
               </div>
             </Col>
-            <Col xs={8} sm={8} md={6} lg={6}>
-              <div className="d-flex align-items-center justify-content-between">
-                <h5>{each.price}</h5>
-              </div>
+            <Col
+              className="d-flex align-items-center justify-content-between"
+              xs={8}
+              sm={8}
+              md={6}
+              lg={6}
+            >
+              <h6 className="ms-1">{each.price}.00$</h6>
+              <span className="plus-minus">
+                <button onClick={() => changeQuantity(each.productId, false)}>
+                  -
+                </button>
+                <span>{each.quantity}</span>
+                <button onClick={() => changeQuantity(each.productId, true)}>
+                  +
+                </button>
+              </span>
+              <h6>{each.price * each.quantity}.00$</h6>
             </Col>
           </Row>
         ))}
 
         <div className="mt-3 d-flex align-items-center justify-content-between">
-          <button className="allbtn space">CONTINUE SHOPPING</button>
-          <button className="allbtn space"> UPADATE CART</button>
+          <Link to="/shop">
+            <button className="allbtn space">CONTINUE SHOPPING</button>
+          </Link>
+          <button onClick={update} className="allbtn space">
+            UPADATE CART
+          </button>
         </div>
         <Row className="my-5 coupon">
           <Col
@@ -74,15 +147,15 @@ const Cart = () => {
               <h5 className="fw-bolder">Cart Total</h5>
               <div className="d-flex align-items-center justify-content-between py-3 border-bottom">
                 <h6 className="fw-bloder">Subtotal</h6>
-                <h6 className="fw-bloder text-danger">$500</h6>
+                <h6 className="fw-bloder text-danger">{price}$</h6>
               </div>
               <div className="d-flex align-items-center justify-content-between py-3 border-bottom">
                 <h6 className="fw-bloder">Tax</h6>
-                <h6 className="fw-bloder text-danger">$100</h6>
+                <h6 className="fw-bloder text-danger">{tax}$</h6>
               </div>
               <div className="d-flex align-items-center justify-content-between py-3">
                 <h6 className="fw-bloder">Total</h6>
-                <h6 className="fw-bloder text-danger">$600</h6>
+                <h6 className="fw-bloder text-danger">0</h6>
               </div>
               <button className="allbtn w-100 space">
                 PROCEED TO CHECKOUT
