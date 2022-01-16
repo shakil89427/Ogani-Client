1422436ce97cf19cf808574dbb259368067e1a43
shakil89427 <shakilahmed89427@gmail.com>
2022-01-16 16:08:30 +0600

    input condition added

--- src/Components/Checkout/Checkout.js
+++ src/Components/Checkout/Checkout.js
@@ -8,6 +8,10 @@ const Checkout = () => {
   const differentAddress = (e) => {
     setAlternative(e.target.checked);
   };
+
+  const placeOrder = (e) => {
+    e.preventDefault();
+  };
   return (
     <div>
       <h1 className="text-center fw-bold checkout-h1">Checkout</h1>
@@ -15,59 +19,66 @@ const Checkout = () => {
         <h3 className="fw-bolder mt-3 border-bottom pb-2 border-2">
           Billing Details
         </h3>
-        <Row>
-          <Col sm={12} md={6} lg={8}>
-            <form>
+        <form onSubmit={placeOrder}>
+          <Row>
+            <Col sm={12} md={6} lg={8}>
               <Row className="billing-form">
                 <Col sm={12} md={12} lg={6}>
                   <p>
-                    First Name <sup>*</sup>{" "}
+                    First Name <sup>*</sup>
                   </p>
-                  <input type="text" />
+                  <input required type="text" />
                 </Col>
                 <Col sm={12} md={12} lg={6}>
                   <p>
-                    Last Name <sup>*</sup>{" "}
+                    Last Name <sup>*</sup>
                   </p>
-                  <input type="text" />
+                  <input required type="text" />
                 </Col>
                 <Col sm={12} md={12} lg={12}>
                   <p>
-                    Country <sup>*</sup>{" "}
+                    Country <sup>*</sup>
                   </p>
-                  <input type="text" />
+                  <input required={!alternative} disabled type="text" />
                 </Col>
                 <Col sm={12} md={12} lg={12}>
                   <p>
-                    Address <sup>*</sup>{" "}
+                    Address <sup>*</sup>
                   </p>
-                  <input placeholder="Street Address" type="text" />
+                  <input
+                    required={!alternative}
+                    disabled
+                    placeholder="Street Address"
+                    type="text"
+                  />
                 </Col>
                 <Col sm={12} md={12} lg={12}>
                   <input
+                    required={!alternative}
+                    disabled
                     placeholder="Apartment,suite,unite ect (optional)"
                     type="text"
                   />
                 </Col>
                 <Col sm={12} md={12} lg={12}>
                   <p>
-                    Town/City <sup>*</sup>{" "}
+                    Town/City <sup>*</sup>
                   </p>
-                  <input type="text" />
+                  <input disabled required={!alternative} type="text" />
                 </Col>
                 <Col sm={12} md={12} lg={12}>
                   <p>
-                    Country/State*
-                    <sup>*</sup>{" "}
+                    Country/State
+                    <sup>*</sup>
                   </p>
-                  <input type="text" />
+                  <input disabled required={!alternative} type="text" />
                 </Col>
                 <Col sm={12} md={12} lg={12}>
                   <p>
-                    Postcode / ZIP*
-                    <sup>*</sup>{" "}
+                    Postcode / ZIP
+                    <sup>*</sup>
                   </p>
-                  <input type="text" />
+                  <input disabled required={!alternative} type="text" />
                 </Col>
                 <Col sm={12} md={12} lg={6}>
                   <p>Phone</p>
@@ -77,7 +88,11 @@ const Checkout = () => {
                   <p>
                     Email <sup>*</sup>
                   </p>
-                  <input type="email" />
+                  <input required type="email" />
+                </Col>
+                <Col sm={12} md={12} lg={12}>
+                  <p>Order Notes</p>
+                  <input type="text" />
                 </Col>
                 <Col sm={12} md={12} lg={12}>
                   <input
@@ -91,22 +106,27 @@ const Checkout = () => {
                 {alternative && (
                   <Col sm={12} md={12} lg={12}>
                     <p>
-                      Country <sup>*</sup>{" "}
+                      Country <sup>*</sup>
                     </p>
-                    <input type="text" />
+                    <input required={alternative} type="text" />
                   </Col>
                 )}
                 {alternative && (
                   <Col sm={12} md={12} lg={12}>
                     <p>
-                      Address <sup>*</sup>{" "}
+                      Address <sup>*</sup>
                     </p>
-                    <input placeholder="Street Address" type="text" />
+                    <input
+                      required={alternative}
+                      placeholder="Street Address"
+                      type="text"
+                    />
                   </Col>
                 )}
                 {alternative && (
                   <Col sm={12} md={12} lg={12}>
                     <input
+                      required={alternative}
                       placeholder="Apartment,suite,unite ect (optional)"
                       type="text"
                     />
@@ -115,81 +135,83 @@ const Checkout = () => {
                 {alternative && (
                   <Col sm={12} md={12} lg={12}>
                     <p>
-                      Town/City <sup>*</sup>{" "}
+                      Town/City <sup>*</sup>
                     </p>
-                    <input type="text" />
+                    <input required={alternative} type="text" />
                   </Col>
                 )}
                 {alternative && (
                   <Col sm={12} md={12} lg={12}>
                     <p>
-                      Country/State*
-                      <sup>*</sup>{" "}
+                      Country/State
+                      <sup>*</sup>
                     </p>
-                    <input type="text" />
+                    <input required={alternative} type="text" />
                   </Col>
                 )}
                 {alternative && (
                   <Col sm={12} md={12} lg={12}>
                     <p>
-                      Postcode / ZIP*
-                      <sup>*</sup>{" "}
+                      Postcode / ZIP
+                      <sup>*</sup>
                     </p>
-                    <input type="text" />
+                    <input required={alternative} type="text" />
                   </Col>
                 )}
               </Row>
-            </form>
-          </Col>
-          <Col sm={12} md={6} lg={4}>
-            <div className="p-4 orders">
-              <h4 className="text-center pb-2 fw-bolder border-bottom border-2">
-                Your Order
-              </h4>
-              <div className="border-bottom">
-                <div className="d-flex align-items-center justify-content-between">
-                  <h5 className="fw-bold">Products</h5>
+            </Col>
+            <Col sm={12} md={6} lg={4}>
+              <div className="p-4 orders">
+                <h4 className="text-center pb-2 fw-bolder border-bottom border-2">
+                  Your Order
+                </h4>
+                <div className="border-bottom">
+                  <div className="d-flex align-items-center justify-content-between">
+                    <h5 className="fw-bold">Products</h5>
+                    <h5 className="fw-bold">Total</h5>
+                  </div>
+                  <div className="d-flex align-items-center justify-content-between">
+                    <p>asedfwqadfe</p>
+                    <p>$500</p>
+                  </div>
+                  <div className="d-flex align-items-center justify-content-between">
+                    <p>asedfwqadfe</p>
+                    <p>$500</p>
+                  </div>
+                  <div className="d-flex align-items-center justify-content-between">
+                    <p>asedfwqadfe</p>
+                    <p>$500</p>
+                  </div>
+                  <div className="d-flex align-items-center justify-content-between">
+                    <p>asedfwqadfe</p>
+                    <p>$500</p>
+                  </div>
+                </div>
+                <div className="mt-2 d-flex align-items-center justify-content-between border-bottom">
+                  <h5 className="fw-bold">Subtotal</h5>
+                  <h5 className="fw-bold">$500</h5>
+                </div>
+                <div className="mt-2 d-flex align-items-center justify-content-between border-bottom">
+                  <h5 className="fw-bold">Tax</h5>
+                  <h5 className="fw-bold">$500</h5>
+                </div>
+                <div className="mt-2 d-flex align-items-center justify-content-between border-bottom">
                   <h5 className="fw-bold">Total</h5>
+                  <h5 className="fw-bold">$500</h5>
                 </div>
-                <div className="d-flex align-items-center justify-content-between">
-                  <p>asedfwqadfe</p>
-                  <p>$500</p>
-                </div>
-                <div className="d-flex align-items-center justify-content-between">
-                  <p>asedfwqadfe</p>
-                  <p>$500</p>
-                </div>
-                <div className="d-flex align-items-center justify-content-between">
-                  <p>asedfwqadfe</p>
-                  <p>$500</p>
-                </div>
-                <div className="d-flex align-items-center justify-content-between">
-                  <p>asedfwqadfe</p>
-                  <p>$500</p>
-                </div>
+                <input
+                  className="me-2 mt-3"
+                  type="checkbox"
+                  name="paymentMethod"
+                />
+                <label htmlFor="paymentMethod">Card Payment</label>
+                <button type="submit" className="mt-3 w-100 allbtn space">
+                  PLACE ORDER
+                </button>
               </div>
-              <div className="mt-2 d-flex align-items-center justify-content-between border-bottom">
-                <h5 className="fw-bold">Subtotal</h5>
-                <h5 className="fw-bold">$500</h5>
-              </div>
-              <div className="mt-2 d-flex align-items-center justify-content-between border-bottom">
-                <h5 className="fw-bold">Tax</h5>
-                <h5 className="fw-bold">$500</h5>
-              </div>
-              <div className="mt-2 d-flex align-items-center justify-content-between border-bottom">
-                <h5 className="fw-bold">Total</h5>
-                <h5 className="fw-bold">$500</h5>
-              </div>
-              <input
-                className="me-2 mt-3"
-                type="checkbox"
-                name="paymentMethod"
-              />
-              <label htmlFor="paymentMethod">Card Payment</label>
-              <button className="mt-3 w-100 allbtn space">PLACE ORDER</button>
-            </div>
-          </Col>
-        </Row>
+            </Col>
+          </Row>
+        </form>
       </Container>
     </div>
   );
