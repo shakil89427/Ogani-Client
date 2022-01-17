f0a1c047b78811e33f850c80c75537e92f44552a
shakil89427 <shakilahmed89427@gmail.com>
2022-01-17 14:31:13 +0600

    login design done

--- src/Components/LoginSignup/Login.js
+++ src/Components/LoginSignup/Login.js
@@ -1,10 +1,52 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./LoginSignup.css";
 
 const Login = () => {
+  const login = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>
-      <h1>login</h1>
+    <div className="login-signup-main">
+      <div className="login-main shadow">
+        <h2 className="login-h2">Login</h2>
+        <div className="p-3">
+          <form onSubmit={login}>
+            <div className="input-div">
+              <i class="fas fa-user"></i>
+              <input required placeholder="Enter your Email" type="email" />
+            </div>
+            <div className="input-div">
+              <i class="fas fa-lock"></i>
+              <input
+                required
+                placeholder="Enter your Password"
+                type="password"
+              />
+            </div>
+            <div className="resetpass">
+              <a href="#">Forgot Password</a>
+            </div>
+            <button type="submit" className="login-signup-btn">
+              Login
+            </button>
+          </form>
+          <p className="my-2 text-center text-secondary">
+            -----------OR-----------
+          </p>
+          <div className="my-2 text-center alternate-sign">
+            <button>
+              <i class="me-2 fab fa-google"></i>Login With Google
+            </button>
+            <p className="mt-5 text-white">
+              New Member?
+              <Link className="newmember" to="signup">
+                Signup
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
