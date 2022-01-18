dcb613b62368982c058e15e33185354540891a84
shakil89427 <shakilahmed89427@gmail.com>
2022-01-19 01:09:31 +0600

    objectid solved

--- src/Components/Hooks/useLoadCartProducts.js
+++ src/Components/Hooks/useLoadCartProducts.js
@@ -3,14 +3,9 @@ import axios from "axios";
 const useLoadCartProducts = () => {
   const loadCartProducts = (cartItems, setCartProducts, setCartLoading) => {
     setCartLoading(true);
-    const ids = [];
-    for (const items of cartItems) {
-      ids.push(items.productId);
-    }
-    console.log(ids);
-    // axios
-    //   .post("http://localhost:5000/allproducts", ids)
-    //   .then((res) => console.log(res.data));
+    axios
+      .post("http://localhost:5000/cartproducts", cartItems)
+      .then((res) => console.log(res.data));
   };
   return { loadCartProducts };
 };
