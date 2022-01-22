29bb7d8d8ce94d9dd197f2c3602b1d749ac82ce5
shakil89427 <shakilahmed89427@gmail.com>
2022-01-22 23:15:48 +0600

    reset toast added

--- src/Components/LoginSignup/Reset.js
+++ src/Components/LoginSignup/Reset.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Spinner } from "react-bootstrap";
 import { isExpired } from "react-jwt";
+import { ToastContainer, toast, Slide } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Reset = () => {
   const [tokenExpired, setTokenExpired] = useState(false);
@@ -34,6 +36,32 @@ const Reset = () => {
 
   const reset = (e) => {
     e.preventDefault();
+    if (e.target[0].value !== e.target[1].value) {
+      return toast.warning("Password Didn't Matched", {
+        position: "top-center",
+        autoClose: 1000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        theme: "colored",
+        transition: Slide,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+    if (e.target[0].value.length < 6) {
+      return toast.warning("Minimum password 6 character", {
+        position: "top-center",
+        autoClose: 1000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        theme: "colored",
+        transition: Slide,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+      });
+    }
     setLoading(true);
     axios
       .post("http://localhost:5000/confirmreset", {
@@ -48,6 +76,7 @@ const Reset = () => {
   };
   return (
     <div className="login-signup-main">
+      <ToastContainer />
       {checkToken && <Spinner animation="border" variant="success" />}
       {tokenExpired && (
         <h2 className="login-h2 rounded-pill px-3">Invalid link</h2>
