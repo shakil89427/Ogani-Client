e4eba869c6dff80ea54c997eed2137898b18cf84
shakil89427 <shakilahmed89427@gmail.com>
2022-01-23 15:01:45 +0600

    coupon removed

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -85,146 +85,120 @@ const Cart = () => {
 
   return (
     <div>
+      {cartLoading && (
+        <div className="text-center my-5">
+          <Spinner className="spin" animation="border" variant="success" />
+        </div>
+      )}
       <ToastContainer />
       <h1 className="text-center fw-bold cart-h1">Shopping Cart</h1>
-      {cartLoading ? (
-        <div className="text-center my-5">
-          <Spinner animation="border" variant="success" />
-        </div>
+
+      {cartItems?.products?.length > 0 ? (
+        <Container className={cartLoading ? "blur" : ""}>
+          <Row className="border-bottom py-2">
+            <Col xs={4} sm={4} md={6} lg={6}>
+              <h5 className="fw-bolder">Products</h5>
+            </Col>
+            <Col
+              className="d-flex align-items-center justify-content-between"
+              xs={8}
+              sm={8}
+              md={6}
+              lg={6}
+            >
+              <h5 className="fw-bolder">Price</h5>
+              <h5 className="fw-bolder">Quantity</h5>
+              <h5 className="fw-bolder">Total</h5>
+            </Col>
+          </Row>
+
+          {cartProducts.map((each) => (
+            <Row key={each._id} className="border-bottom py-2">
+              <Col xs={4} sm={4} md={6} lg={6}>
+                <div className="d-flex flex-column flex-md-row align-items-center">
+                  <button
+                    onClick={() => removefromCart(each._id)}
+                    className="border-0 me-2 mb-2"
+                  >
+                    X
+                  </button>
+                  <img className="cart-img me-3 mb-2" src={each.img} alt="" />
+                  <h6>{each.name}</h6>
+                </div>
+              </Col>
+              <Col
+                className="d-flex align-items-center justify-content-between"
+                xs={8}
+                sm={8}
+                md={6}
+                lg={6}
+              >
+                <h6 className="ms-1">{parseFloat(each.price).toFixed(2)}$</h6>
+                <span className="plus-minus">
+                  <button onClick={() => changeQuantity(each._id, false)}>
+                    -
+                  </button>
+                  <span>{each.quantity}</span>
+                  <button onClick={() => changeQuantity(each._id, true)}>
+                    +
+                  </button>
+                </span>
+
+                <h6>{parseFloat(each.price * each.quantity).toFixed(2)}$</h6>
+              </Col>
+            </Row>
+          ))}
+
+          <div className="mt-3 d-flex align-items-center justify-content-between">
+            <Link to="/shop">
+              <button className="allbtn space">SHOP</button>
+            </Link>
+            <button onClick={update} className="allbtn space">
+              UPADATE CART
+            </button>
+          </div>
+          <Row className="my-5 coupon">
+            {!cartLoading && cartItems?.products?.length > 0 && (
+              <Col className="ms-auto" sm={12} md={6} lg={6}>
+                <div className="cart-total">
+                  <h5 className="fw-bolder">Cart Total</h5>
+                  <div className="d-flex align-items-center justify-content-between py-3 border-bottom">
+                    <h6 className="fw-bloder">Subtotal</h6>
+                    <h6 className="fw-bloder text-danger">
+                      {parseFloat(subtotal).toFixed(2)}$
+                    </h6>
+                  </div>
+                  <div className="d-flex align-items-center justify-content-between py-3 border-bottom">
+                    <h6 className="fw-bloder">Tax</h6>
+                    <h6 className="fw-bloder text-danger">
+                      {parseFloat(tax).toFixed(2)}$
+                    </h6>
+                  </div>
+                  <div className="d-flex align-items-center justify-content-between py-3">
+                    <h6 className="fw-bloder">Total</h6>
+                    <h6 className="fw-bloder text-danger">
+                      {parseFloat(total).toFixed(2)}$
+                    </h6>
+                  </div>
+                  <Link to="/checkout">
+                    <button className="allbtn w-100 space">
+                      PROCEED TO CHECKOUT
+                    </button>
+                  </Link>
+                </div>
+              </Col>
+            )}
+          </Row>
+        </Container>
       ) : (
         <div>
-          {!cartLoading && cartItems?.products?.length === 0 && (
+          {!cartLoading && (
             <div className="p-5 d-flex flex-column align-items-center justify-content-center">
               <h5>You didn't added any product to Cart</h5>
               <Link to="/shop">
                 <button className="allbtn">Continue To Shop</button>
               </Link>
             </div>
-          )}
-          {cartItems?.products?.length > 0 && (
-            <Container>
-              <Row className="border-bottom py-2">
-                <Col xs={4} sm={4} md={6} lg={6}>
-                  <h5 className="fw-bolder">Products</h5>
-                </Col>
-                <Col
-                  className="d-flex align-items-center justify-content-between"
-                  xs={8}
-                  sm={8}
-                  md={6}
-                  lg={6}
-                >
-                  <h5 className="fw-bolder">Price</h5>
-                  <h5 className="fw-bolder">Quantity</h5>
-                  <h5 className="fw-bolder">Total</h5>
-                </Col>
-              </Row>
-
-              {cartProducts.map((each) => (
-                <Row key={each._id} className="border-bottom py-2">
-                  <Col xs={4} sm={4} md={6} lg={6}>
-                    <div className="d-flex flex-column flex-md-row align-items-center">
-                      <button
-                        onClick={() => removefromCart(each._id)}
-                        className="border-0 me-2 mb-2"
-                      >
-                        X
-                      </button>
-                      <img
-                        className="cart-img me-3 mb-2"
-                        src={each.img}
-                        alt=""
-                      />
-                      <h6>{each.name}</h6>
-                    </div>
-                  </Col>
-                  <Col
-                    className="d-flex align-items-center justify-content-between"
-                    xs={8}
-                    sm={8}
-                    md={6}
-                    lg={6}
-                  >
-                    <h6 className="ms-1">
-                      {parseFloat(each.price).toFixed(2)}$
-                    </h6>
-                    <span className="plus-minus">
-                      <button onClick={() => changeQuantity(each._id, false)}>
-                        -
-                      </button>
-                      <span>{each.quantity}</span>
-                      <button onClick={() => changeQuantity(each._id, true)}>
-                        +
-                      </button>
-                    </span>
-
-                    <h6>
-                      {parseFloat(each.price * each.quantity).toFixed(2)}$
-                    </h6>
-                  </Col>
-                </Row>
-              ))}
-
-              <div className="mt-3 d-flex align-items-center justify-content-between">
-                <Link to="/shop">
-                  <button className="allbtn space">SHOP</button>
-                </Link>
-                <button onClick={update} className="allbtn space">
-                  UPADATE CART
-                </button>
-              </div>
-              <Row className="my-5 coupon">
-                <Col
-                  className="mb-3 d-flex flex-column align-items-center justify-content-center"
-                  sm={12}
-                  md={6}
-                  lg={6}
-                >
-                  <h5 className="fw-bolder">Discount Codes</h5>
-                  <form className="text-center" onSubmit={applyCoupon}>
-                    <input
-                      className="w-100"
-                      required
-                      placeholder="Enter here"
-                      type="text"
-                    />
-                    <button type="submit" className="allbtn space">
-                      APPLY COUPON
-                    </button>
-                  </form>
-                </Col>
-                {!cartLoading && cartItems?.products?.length > 0 && (
-                  <Col sm={12} md={6} lg={6}>
-                    <div className="cart-total">
-                      <h5 className="fw-bolder">Cart Total</h5>
-                      <div className="d-flex align-items-center justify-content-between py-3 border-bottom">
-                        <h6 className="fw-bloder">Subtotal</h6>
-                        <h6 className="fw-bloder text-danger">
-                          {parseFloat(subtotal).toFixed(2)}$
-                        </h6>
-                      </div>
-                      <div className="d-flex align-items-center justify-content-between py-3 border-bottom">
-                        <h6 className="fw-bloder">Tax</h6>
-                        <h6 className="fw-bloder text-danger">
-                          {parseFloat(tax).toFixed(2)}$
-                        </h6>
-                      </div>
-                      <div className="d-flex align-items-center justify-content-between py-3">
-                        <h6 className="fw-bloder">Total</h6>
-                        <h6 className="fw-bloder text-danger">
-                          {parseFloat(total).toFixed(2)}$
-                        </h6>
-                      </div>
-                      <Link to="/checkout">
-                        <button className="allbtn w-100 space">
-                          PROCEED TO CHECKOUT
-                        </button>
-                      </Link>
-                    </div>
-                  </Col>
-                )}
-              </Row>
-            </Container>
           )}
         </div>
       )}
