417ca6d9775f6320bea4094ed268c064ec162ff4
shakil89427 <shakilahmed89427@gmail.com>
2022-01-25 14:18:17 +0600

    loadcart async await added

--- src/Components/Hooks/useLoadCart.js
+++ src/Components/Hooks/useLoadCart.js
@@ -4,25 +4,28 @@ import useSetToLocal from "./useSetToLocal";
 const useLoadCart = () => {
   const { setToLocal } = useSetToLocal();
 
-  const loadCart = (user, setCartItems, result) => {
+  const loadCart = async (user, setCartItems, result) => {
     if (user?._id) {
-      axios.get(`http://localhost:5000/getcart/${user._id}`).then((res) => {
-        if (res.data) {
-          setCartItems(res.data);
+      try {
+        const response = await axios.get(
+          `http://localhost:5000/getcart/${user._id}`
+        );
+        if (response?.data) {
+          setCartItems(response.data);
           return localStorage.removeItem("cart");
         }
-        if (!res.data && result) {
+        if (!response?.data && result) {
           result._id = user._id;
           setToLocal(user, setCartItems, result);
           return localStorage.removeItem("cart");
         }
-        if (!res.data && !result) {
+        if (!response?.data && !result) {
           setToLocal(user, setCartItems, {
             _id: user._id,
             products: [],
           });
         }
-      });
+      } catch (error) {}
     }
     if (!user?._id && !result) {
       return setToLocal(user, setCartItems, { _id: "user", products: [] });
